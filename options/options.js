7989057e29bc9fbd9c0e5ac960d5de2fdbb7fc12
Crowdbotics <team@crowdbotics.com>
2024-04-17 14:24:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-union-47569",
+  url: "https://spring-union-47569.botics.co",
+  api: "https://spring-union-47569.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
